86b26686228a3b13af6e0087295ff3ffedfd1108
Taylor Facen <taylor.facen@gmail.com>
2020-04-11 12:47:26 -0500

    Get History

--- nomics/api/exchange_rates.py
+++ nomics/api/exchange_rates.py
@@ -3,7 +3,7 @@ import requests
 from .api import API
 
 class ExchangeRates(API):
-    def get_exchange_rates(self, format = None):
+    def get_rates(self, format = None):
         '''
         Returns the current exchange rates used by Nomics to convert prices from markets into USD. 
         This contains Fiat currencies as well as a BTC and ETH quote prices.
@@ -15,6 +15,31 @@ class ExchangeRates(API):
         url = self.client.get_url('exchange-rates')
         params = {
             'format': format
+        }
+
+        resp = requests.get(url, params = params)
+
+        if resp.status_code == 200:
+            return resp.json()
+        else:
+            return resp.text
+
+    def get_history(self, currency, start, end = None):
+        '''
+        Returns exchange rates for every point in a time range.
+
+        :param  str     currency:   Currency ID
+
+        :param  str     start:      Start time of the interval in RFC3339 format
+
+        :param  str     end:        End time of the interval in RFC3339 format. If not provided, the current time is used. 
+        '''
+
+        url = self.client.get_url('exchange-rates/history')
+        params = {
+            'currency': currency,
+            'start': start,
+            'end': end
         }
 
         resp = requests.get(url, params = params)
--- tests/test_exchange_rates.py
+++ tests/test_exchange_rates.py
@@ -8,7 +8,15 @@ from nomics import Nomics
 def nomics():
     return Nomics(NOMICS_API_KEY)
 
-def test_get_exchange_rates(nomics):
-    data = nomics.ExchangeRates.get_exchange_rates()
+def test_get_rates(nomics):
+    data = nomics.ExchangeRates.get_rates()
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+def test_get_history(nomics):
+    data = nomics.ExchangeRates.get_history(
+        currency = "ETH",
+        start = "2018-04-14T00:00:00Z"
+    )
     assert isinstance(data, list)
     assert len(data) > 0
