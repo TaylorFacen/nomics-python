6b02bcedafea7a0f96f24236a850522e3664e77f
Taylor Facen <taylor.facen@gmail.com>
2020-04-12 12:55:34 -0500

    Remove unnecessary packages from requirements

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
-    name="nomics-python",
-    version="3.0.0",
-    author="Taylor Facen",
-    author_email="taylor.facen@gmail.com",
-    description="A python wrapper for the Nomics API",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/TaylorFacen/nomics-python",
-    packages=setuptools.find_packages(),
-    classifiers=[
+    name = "nomics-python",
+    version = "3.0.2",
+    author = "Taylor Facen",
+    author_email = "taylor.facen@gmail.com",
+    description = "A python wrapper for the Nomics API",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/TaylorFacen/nomics-python",
+    packages = setuptools.find_packages(),
+    install_requires = required,
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
     ]
