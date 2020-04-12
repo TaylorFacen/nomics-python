471396eb15bdd67785e9bda3ea9e0dcf2f025c20
Taylor Facen <taylor.facen@gmail.com>
2020-04-12 13:46:02 -0500

    Explicitly list requests as dependency

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setuptools.setup(
     name = "nomics-python",
-    version = "3.0.2",
+    version = "3.0.3",
     author = "Taylor Facen",
     author_email = "taylor.facen@gmail.com",
     description = "A python wrapper for the Nomics API",
@@ -16,7 +13,7 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url = "https://github.com/TaylorFacen/nomics-python",
     packages = setuptools.find_packages(),
-    install_requires = required,
+    install_requires = ['requests>=2'],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
